e9f5c51f80614bf48782ee71e4b92832959966fd
daniel-budai <danielbudai@tutanota.com>
2024-10-29 20:03:14 +0100

    review page

--- src/components/MeetupCard/MeetupCard.jsx
+++ src/components/MeetupCard/MeetupCard.jsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import "./meetupCard.scss";
 
-const MeetupCard = ({ id, title, description }) => {
+const MeetupCard = ({ id, title, description, category }) => {
   const navigate = useNavigate();
 
   return (
     <div className="meetupCard" onClick={() => navigate(`/meetups/${id}`)}>
       <h3 className="meetupCard__title">{title}</h3>
+      <span className="meetupCard__category">{category}</span>
       <p className="meetupCard__description">{description}</p>
     </div>
   );
