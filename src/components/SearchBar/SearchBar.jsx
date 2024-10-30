6a8de799726e7fa40b12554c9afbc6da79e9be5a
Emadenni <emanyele.dng86@gmail.com>
2024-10-31 00:37:56 +0100

    create results component and hook useSearch

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,27 @@
-import "./searchBar.scss";
+import React from 'react';
+import useSearch from '../../hooks/useSearch';
+import SearchResults from '../SearchResults/SearchResults';
+import "./searchBar.scss"
 
 const SearchBar = () => {
+  const { results, loading, error, search } = useSearch();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const query = event.target.elements.query.value;
+    search(query);
+  };
+
   return (
     <div className="searchBarContainer">
-      <input type="text" className="searchBarInput" placeholder="Search..." />
-      <button className="searchBarButton">Search</button>
+      <form onSubmit={handleSearch}>
+        <input type="text" name="query" className="searchBarInput" placeholder="Search..." />
+        <button className="searchBarButton" type="submit">Search</button>
+      </form>
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {!loading && results.length === 0 }
+      {results.length > 0 && <SearchResults results={results} />}
     </div>
   );
 };
---
+++ src/hooks/useSearch.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const useSearch = () => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const search = async (query) => {
+    setLoading(true);
+    setError(null);
+
+    if (!query || query.length < 1) {
+        setError("keyword must be at least one character ");
+        setLoading(false);
+        return;
+      }
+  
+    const endpoint = `https://uw8qzn03l8.execute-api.eu-north-1.amazonaws.com/meetups/search?keyword=${encodeURIComponent(query)}`;
+
+  
+    try {
+      console.log("Invio richiesta a:", endpoint);
+      const response = await fetch(endpoint, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("Stato risposta:", response.status);
+      if (!response.ok) {
+        const errorText = await response.text(); 
+        throw new Error(`Error: ${response.status} - ${errorText}`);
+      }
+  
+      const data = await response.json();
+      console.log(data);
+      
+      setResults(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+
+  return { results, loading, error, search };
+};
+
+export default useSearch;
