62940ce4adcfc7f17bd180fcadf97d95bca205ff
Emadenni <emanyele.dng86@gmail.com>
2024-10-30 21:25:30 +0100

    styled logout button and included error message

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -8,18 +8,19 @@ const LoginForm = ({ toggleOverlay }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
     console.log("Login attempt with:", { email, password });
 
-    const endpoint = 'https://uw8qzn03l8.execute-api.eu-north-1.amazonaws.com/login';
+    const endpoint = "https://jsonplaceholder.typicode.com/posts";
 
     try {
       const response = await fetch(endpoint, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ email, password }),
       });
@@ -32,14 +33,15 @@ const LoginForm = ({ toggleOverlay }) => {
       console.log("Response data:", data);
 
       if (data.success && data.token) {
-        sessionStorage.setItem('token', data.token);
-        console.log("Token saved in session storage.");
+        sessionStorage.setItem("token", data.token);
+        setError("");
         alert("Login successful!");
       } else {
         throw new Error("Login failed: invalid response data.");
       }
     } catch (error) {
       console.error("Login failed:", error);
+      setError("Login failed. Please check your credentials and try again.");
     }
   };
 
@@ -48,12 +50,13 @@ const LoginForm = ({ toggleOverlay }) => {
   };
 
   const handleLogout = () => {
-    sessionStorage.removeItem('token');
+    sessionStorage.removeItem("token");
     alert("Token removed!");
   };
 
   return (
     <div className="loginForm-container">
+      <FormButton type="button" text="Logout" className="logout-button" onClick={handleLogout} />
       <h2>Start Your Journey</h2>
       <form onSubmit={handleLogin}>
         <TextField
@@ -85,12 +88,10 @@ const LoginForm = ({ toggleOverlay }) => {
             ),
           }}
         />
+        {error && <p className="error-message">{error}</p>}
         <FormButton type="submit" text="Login" className="login-button" />
         <FormButton type="button" text="Sign Up" className="overlayToggle-button" onClick={toggleOverlay} />
       </form>
-      <button onClick={handleLogout} style={{ marginTop: '10px' }}>
-        Remove Token (Logout)
-      </button>
     </div>
   );
 };
