62b0203c470eef3641f07e73cf52006f176ac385
daniel-budai <danielbudai@tutanota.com>
2024-10-30 13:49:51 +0100

    started hooks

--- src/components/MeetupBoard/MeetupBoard.jsx
+++ src/components/MeetupBoard/MeetupBoard.jsx
@@ -7,7 +7,13 @@ const MeetupBoard = ({ meetups }) => {
       <h2 className="meetupBoard__title">Upcoming Meetups</h2>
       <div className="meetupBoard__grid">
         {meetups.map((meetup) => (
-          <MeetupCard key={meetup.id} {...meetup} />
+          <MeetupCard
+            key={meetup.meetupId}
+            meetupId={meetup.meetupId}
+            title={meetup.title}
+            category={meetup.category}
+            description={meetup.description}
+          />
         ))}
       </div>
     </div>
