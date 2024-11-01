0627962cae6aebea004fbe3e808934c62f1e16d4
Emadenni <emanyele.dng86@gmail.com>
2024-11-01 20:12:39 +0100

    corrected meetups handling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useFilters } from "../../hooks/useFilters";
+import { useMeetups } from "../../hooks/useMeetups"; // Importa l'hook qui
 import Header from "../../components/Header/Header";
 import MeetupBoard from "../../components/MeetupBoard/MeetupBoard";
 import FilterSearch from "../../components/FilterSearch/FilterSearch";
@@ -9,34 +10,10 @@ import "./home.scss";
 
 export default function Home() {
   const { filters, updateFilters } = useFilters();
+  const { meetups, loading, error } = useMeetups(); // Utilizza l'hook per ottenere meetups, loading ed error
   const [filterVisible, setFilterVisible] = useState(false);
   const [filteredMeetups, setFilteredMeetups] = useState([]);
   const [showFiltered, setShowFiltered] = useState(false);
-  const [meetups, setMeetups] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  const fetchMeetups = async () => {
-    setLoading(true);
-    const url = "https://2wwh49b9bf.execute-api.eu-north-1.amazonaws.com/meetups";
-
-    try {
-      const response = await fetch(url);
-      if (!response.ok) throw new Error("Network response was not ok");
-
-      const data = await response.json();
-      setMeetups(data.meetups);
-      setError("");
-    } catch (err) {
-      setError(err.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchMeetups();
-  }, []);
 
   const handleSearch = (newFilters) => {
     updateFilters(newFilters);
